bc585d35693c45b63cb47f4a5300428151e1996c
FaithDakoru <133269309+FaithDakoru@users.noreply.github.com>
2023-08-24 10:19:12 +0600

    Update proc_file_comm.c
    Team project

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -4,9 +4,9 @@ int cant_open(char *file_path);
 int proc_file_commands(char *file_path, int *exe_ret);
 
 /**
- * cant_open - If the file doesn't exist or lacks proper permissions, print
+ * cant_open - That is if the file doesn't exist or lacks proper permissions, print
  * a cant open error.
- * @file_path: Path to the supposed file.
+ * @file_path: The path to the supposed file.
  *
  * Return: 127.
  */

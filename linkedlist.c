1939228a7d4353a0652962ff79295257a59e11ed
FaithDakoru <133269309+FaithDakoru@users.noreply.github.com>
2023-08-24 10:13:52 +0600

    Update linkedlist.c
    Team project

--- linkedlist.c
+++ linkedlist.c
@@ -6,10 +6,10 @@ list_t *add_node_end(list_t **head, char *dir);
 void free_list(list_t *head);
 
 /**
- * add_alias_end - Adds a node to the end of a alias_t linked list.
- * @head: A pointer to the head of the list_t list.
- * @name: The name of the new alias to be added.
- * @value: The value of the new alias to be added.
+ * add_alias_end - This adds a node to the end of a alias_t linked list.
+ * @head: This is a pointer to the head of the list_t list.
+ * @name: This is the name of the new alias to be added.
+ * @value: This is the value of the new alias to be added.
  *
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
